c2f99c9bc11b2a60bb489b621797ea99fbfd1875
Anastasia Kroshkina <nastikak@yandex.ru>
2025-04-08 16:51:13 +0300

    Задание по basic-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,25 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    const romanMap = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+
+    for (let i = 0; i < str.length; i++) {
+        const current = romanMap[str[i]];
+        const next = romanMap[str[i + 1]];
+
+        if (next && current < next) {
+            result += next - current;
+            i++;
+        } else {
+            result += current;
+        }
+    }
     return result;
 }
